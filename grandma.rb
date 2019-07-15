283cc5ecb0a225e15af74ef1dca4828866c93580
David Little <littledavid175@gmail.com>
2019-07-15 17:44:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(hello)
   if hello == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif hello == "WHAT DID YOU EAT TODAY?"
-  
+    "NO, NOT SINCE 1938!"
   else 
   "HUH?! SPEAK UP, SONNY!"
   end
